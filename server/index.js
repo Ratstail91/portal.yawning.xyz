04fa17b1d23c4fbcf31fdf7be512a5a7e9388747
Alexis Tyler <xo@wvvw.me>
2018-01-09 15:37:29 +1030

    allow MYSQL_USERNAME to use ENV

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(expressFormidable());
 // Db connections
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'node',
+  user: process.env.MYSQL_USERNAME || 'node',
   password: process.env.MYSQL_PASSWORD || fs.readFileSync('../node.pwd', 'utf8').replace(/^\s+|\s+$/g, '')
 });
 
